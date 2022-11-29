3919a529eda732d0496f21342db680f090b11ba5
c0a21099 <c0a21099e4@edu.teu.ac.jp>
2022-11-29 14:21:27 +0900

    練習問題2完了.

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,4 +5,18 @@ root = tk.Tk()
 root.title("calc_GUI")
 root.geometry("300x500")
 
+#2. ボタンの作成
+
+r = 0
+c = 0
+
+for num in range(9, -1, -1):
+    button = tk.Button(root, text=num, width = 4, height = 2,
+                       font = ("", 30))
+    button.grid(row=r, column=c)
+    c += 1
+    if c % 3 == 0:
+        r += 1
+        c = 0
+
 root.mainloop()

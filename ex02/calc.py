189d4d3d537f909a5301443a343d191a472c7a4f
c0a21099 <c0a21099e4@edu.teu.ac.jp>
2022-11-29 14:25:16 +0900

    練習問題3完了 & 練習2見やすさ改善.

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+#3. クリックした際の反応
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt, f"[{txt}]ボタンが押されました.")
 
 # 1. ウィンドウの作成
 root = tk.Tk()
@@ -14,9 +21,14 @@ for num in range(9, -1, -1):
     button = tk.Button(root, text=num, width = 4, height = 2,
                        font = ("", 30))
     button.grid(row=r, column=c)
+
     c += 1
+
     if c % 3 == 0:
         r += 1
         c = 0
+
+    #クリック時の処理
+    button.bind("<1>", button_click)
 
 root.mainloop()

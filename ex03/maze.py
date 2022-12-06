f88486d260091cad1c0e39110a7743bcacad709e
c0a21099 <c0a21099e4@edu.teu.ac.jp>
2022-12-06 14:28:32 +0900

    練習3-3まで.

--- ex03/maze.py
+++ ex03/maze.py
@@ -6,5 +6,12 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
+    
+    # 画像描画
+    cx, cy = 300, 400
+    tori = tk.PhotoImage(file = "../fig/8.png")
+    canvas.create_image(cx, cy,
+                        image = tori, 
+                        tag = "kokaton")
     canvas.pack()
     root.mainloop()

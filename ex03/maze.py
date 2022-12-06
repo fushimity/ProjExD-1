c9389ed9af398a8cac895e6821f8ece84bf7bb12
c0a21099 <c0a21099e4@edu.teu.ac.jp>
2022-12-06 14:33:39 +0900

    練習3-5 & 練習3-6まで.

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,6 +2,15 @@
 
 import tkinter as tk
 
+
+def key_down(event):
+    global key
+    key = event.keysym
+
+def key_up(event):
+    global key
+    key = ""
+
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -15,4 +24,6 @@ if __name__ == '__main__':
                         tag = "kokaton")
     canvas.pack()
     key = ""
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
     root.mainloop()

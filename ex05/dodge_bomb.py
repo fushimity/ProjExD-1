77431397140e6af8afd63987c13057f740491b0b
c0a21099 <c0a21099e4@edu.teu.ac.jp>
2022-12-20 14:38:06 +0900

    練習5-2まで.

--- ex05/dodge_bomb.py
+++ ex05/dodge_bomb.py
@@ -7,23 +7,46 @@ import sys
 class Screen():
     def __init__(self, title, wh, img_path):
         # 練習１
-        pg.display.set_caption(title)           # "逃げろ！こうかとん"
-        self.sfc = pg.display.set_mode(wh)      #(1600, 900)(幅/高さのタプル)
+        pg.display.set_caption(title)               # "逃げろ！こうかとん"
+        self.sfc = pg.display.set_mode(wh)          #(1600, 900)(幅/高さのタプル)
         self.rct = self.rct.get_rect()
         self.bgi_sfc = pg.image.load(img_path)      # "../fig/pg_bg.jpg"
         self.bgi_rct = self.bgi_rect.get_rect()
 
     def blit(self):
         # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける
-        self.sfc.blit(self.bgi_sfc, self.bgi_rct) 
+        self.sfc.blit(self.bgi_sfc, self.bgi_rct)
 
 
-key_delta = {
-    pg.K_UP:    [0, -1],
-    pg.K_DOWN:  [0, +1],
-    pg.K_LEFT:  [-1, 0],
-    pg.K_RIGHT: [+1, 0],
-}
+class Bird():
+    key_delta = {               # クラス変数として設定.
+        pg.K_UP:    [0, -1],
+        pg.K_DOWN:  [0, +1],
+        pg.K_LEFT:  [-1, 0],
+        pg.K_RIGHT: [+1, 0],
+    }
+
+    def __init__(self, img_path, ratio, xy):
+        # 練習３
+        self.sfc = pg.image.load(img_path)                        # "../fig/6.png"
+        self.fc = pg.transform.rotozoom(self._sfc, 0, ratio)      # 拡大率 -> ratio(2.0), (他の関数で使わないからselfつけない.)
+        self.rct = self.sfc.get_rect()
+        self.rct.center = xy
+
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr:Screen):
+        key_dct = pg.key.get_pressed()
+        for key, delta in Bird.key_delta.items(): # key -> どのキー, delta -> 移動方向
+            if key_dct[key]:
+                self.rct.centerx += delta[0]
+                self.rct.centery += delta[1]
+            # 練習7
+            if check_bound(self.rct, scr.rct) != (+1, +1):  # どこかしらはみ出ていたら...
+                self.rct.centerx -= delta[0]
+                self.rct.centery -= delta[1]
+        self.blit(scr)
 
 
 def check_bound(obj_rct, scr_rct):
@@ -47,12 +70,9 @@ def main():
     scr = Screen("逃げろ！こうかとん", (1600, 900), "../fig/pg_bg.jpg")
 
     # 練習３
-    tori_sfc = pg.image.load("../fig/6.png")
-    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
-    tori_rct = tori_sfc.get_rect()
-    tori_rct.center = 900, 400
+    kkt = Bird("../fig/6.png", 2.0, (900, 400))         # こうかとんの画像
     # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける
-    scrn_sfc.blit(tori_sfc, tori_rct) 
+    kkt.update(scr)
 
     # 練習５
     bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
@@ -73,16 +93,16 @@ def main():
             if event.type == pg.QUIT:
                 return
 
-        key_dct = pg.key.get_pressed()
-        for key, delta in key_delta.items():
-            if key_dct[key]:
-                tori_rct.centerx += delta[0]
-                tori_rct.centery += delta[1]
-            # 練習7
-            if check_bound(tori_rct, scrn_rct) != (+1, +1):
-                tori_rct.centerx -= delta[0]
-                tori_rct.centery -= delta[1]
-        scrn_sfc.blit(tori_sfc, tori_rct) # 練習3
+        # key_dct = pg.key.get_pressed()
+        # for key, delta in key_delta.items():
+        #     if key_dct[key]:
+        #         tori_rct.centerx += delta[0]
+        #         tori_rct.centery += delta[1]
+        #     # 練習7
+        #     if check_bound(tori_rct, scrn_rct) != (+1, +1):
+        #         tori_rct.centerx -= delta[0]
+        #         tori_rct.centery -= delta[1]
+        kkt.update(scr)
 
         # 練習６
         bomb_rct.move_ip(vx, vy)

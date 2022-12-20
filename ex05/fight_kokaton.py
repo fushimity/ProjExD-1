bd2879e2b2e29e84b37de388b8aff8b6af66296e
c0a21099 <c0a21099e4@edu.teu.ac.jp>
2022-12-20 17:38:55 +0900

    4限追加機能実装完了.

--- ex05/fight_kokaton.py
+++ ex05/fight_kokaton.py
@@ -1,10 +1,32 @@
-# アルファベット順にパッケージ読み出し
 import pygame as pg
+import os
 import random
 import sys
 
 
-class Screen():
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+# 攻撃部分
+MAX_SHOTS = 10
+# fluescreen
+SCREENRECT = pg.Rect(0, 0, 640, 480)
+#time 計算
+SCORE = 0
+
+
+# imageのロード(New)
+class Setup:
+    def load_image():
+        """loads an image, prepares it for play"""
+        file = os.path.join(main_dir, "background.gif", file)
+        try:
+            surface = pg.image.load(file)
+        except pg.error:
+            raise SystemExit('Could not load image "%s" %s' % (file, pg.get_error()))
+        return surface.convert()
+
+
+class Screen:
     def __init__(self, title, wh, img_path):
         pg.display.set_caption(title) 
         self.sfc = pg.display.set_mode(wh)
@@ -13,12 +35,11 @@ class Screen():
         self.bgi_rct = self.bgi_sfc.get_rect() 
 
     def blit(self):
-        self.sfc.blit(self.bgi_sfc, self.bgi_rct)
+        self.sfc.blit(self.bgi_sfc, self.bgi_rct) 
 
 
-class Bird():
-    # クラス変数として設定.
-    key_delta = {              
+class Bird:
+    key_delta = {
         pg.K_UP:    [0, -1],
         pg.K_DOWN:  [0, +1],
         pg.K_LEFT:  [-1, 0],
@@ -26,8 +47,8 @@ class Bird():
     }
 
     def __init__(self, img_path, ratio, xy):
-        self.sfc = pg.image.load(img_path)                        # "../fig/6.png"
-        self.sfc = pg.transform.rotozoom(self.sfc, 0, ratio)      # 拡大率 -> ratio(2.0), (他の関数で使わないからselfつけない.)
+        self.sfc = pg.image.load(img_path)
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, ratio)
         self.rct = self.sfc.get_rect()
         self.rct.center = xy
 
@@ -36,17 +57,17 @@ class Bird():
 
     def update(self, scr:Screen):
         key_dct = pg.key.get_pressed()
-        for key, delta in Bird.key_delta.items(): # key -> どのキー, delta -> 移動方向
+        for key, delta in Bird.key_delta.items():
             if key_dct[key]:
                 self.rct.centerx += delta[0]
-                self.rct.centery += delta[1]
-            if check_bound(self.rct, scr.rct) != (+1, +1):  # どこかしらはみ出ていたら...
+                self.rct.centery += delta[1]  
+            if check_bound(self.rct, scr.rct) != (+1, +1):
                 self.rct.centerx -= delta[0]
                 self.rct.centery -= delta[1]
-        self.blit(scr)
+        self.blit(scr)                    
 
 
-class Bomb():
+class Bomb:
     def __init__(self, color, rad, vxy, scr:Screen):
         self.sfc = pg.Surface((2*rad, 2*rad)) # 正方形の空のSurface
         self.sfc.set_colorkey((0, 0, 0))
@@ -67,6 +88,47 @@ class Bomb():
         self.blit(scr)
 
 
+# 攻撃機能を持たせる(New)
+class Shot():
+    """a bullet the Player sprite fires."""
+
+    speed = -11
+    images = []
+
+    def __init__(self, pos):
+        pg.sprite.Sprite.__init__(self, self.containers)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect(midbottom=pos)
+
+    def update(self):
+        """called every time around the game loop.
+
+        Every tick we move the shot upwards.
+        """
+        self.rect.move_ip(0, self.speed)
+        if self.rect.top <= 0:
+            self.kill()
+
+
+# 時間をすぎるごとにScoreが加算されるようにする(New)
+class Score:
+    """to keep track of the score."""
+
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.font = pg.font.Font(None, 20)
+        self.font.set_italic(1)
+        self.color = "white"
+        self.lastscore = -1
+        self.update()
+        self.rect = self.image.get_rect().move(10, 450)
+
+    def update(self):
+        self.lastscore = SCORE
+        msg = "Score: %d" % SCORE
+        self.image = self.font.render(msg, 0, self.color)
+
+
 def check_bound(obj_rct, scr_rct):
     """
     第1引数：こうかとんrectまたは爆弾rect
@@ -81,38 +143,88 @@ def check_bound(obj_rct, scr_rct):
     return yoko, tate
 
 
+def load_sound(file):
+    """because pygame can be be compiled without mixer."""
+    if not pg.mixer:
+        return None
+    file = os.path.join(main_dir, "data", file)
+    try:
+        sound = pg.mixer.Sound(file)
+        return sound
+    except pg.error:
+        print("Warning, unable to load, %s" % file)
+    return None
+
+
 def main():
+    if pg.get_sdl_version()[0] == 2:
+        pg.mixer.pre_init(44100, 32, 2, 1024)
+    pg.init()
+    if pg.mixer and not pg.mixer.get_init():
+        print("Warning, no sound")
+        pg.mixer = None
+
+    boom_sound = load_sound("boom.wav")
+    shoot_sound = load_sound("car_door.wav")
+    if pg.mixer:
+        music = os.path.join(main_dir, "data", "house_lo.wav")
+        pg.mixer.music.load(music)
+        pg.mixer.music.play(-1)
+
     clock =pg.time.Clock()
-    
-    # 練習1
-    scr = Screen("逃げろ！こうかとん", (1600, 900), "../fig/pg_bg.jpg")
+
+
+    Shot.images = Setup()
+
+    # <追加分>
+    global SCORE
+    fullscreen = False
+
+    # 練習１
+    scr = Screen("逃げろ！こうかとん", (1600,900), "../fig/pg_bg.jpg")
 
     # 練習３
-    kkt = Bird("../fig/6.png", 2.0, (900, 400))         # こうかとんの画像
+    kkt = Bird("../fig/6.png", 2.0, (900,400))
     kkt.update(scr)
 
     # 練習５
-    bkd = Bomb((255, 0, 0), 10, (+1, +1), scr)
-    bkd.update(scr)
+    bkd_lst = []
+    color_lst = ["red", "green", "blue", "yellow", "magenta"]
+    for i in range(20):
+        bkd = Bomb(color_lst[i%5], 10, (random.choice(range(-2, 3)), random.choice(range(-2, 3))), scr)
+        bkd_lst.append(bkd)
+    # bkd.update(scr)
+
+    clock_change = 1000
 
     # 練習２
-    while True:
+    while True:        
         scr.blit()
-
+        Score()
         for event in pg.event.get():
+            SCORE = SCORE + 1           # Scoreを+1ずつ行う
+            print(SCORE)
+            # Shot() 
+            if event.type == pg.K_f:
+                if not fullscreen:
+                    print("Changing to FULLSCREEN")
+                    # 未実装...
+            if event.type == pg.K_SPACE:
+                print("Push the spacekey.")
+                clock_change = 50
             if event.type == pg.QUIT:
                 return
-
         kkt.update(scr)
-        bkd.update(scr)
 
-        # 練習８
-        if kkt.rct.colliderect(bkd.rct):
-            return
+        for i in range(len(bkd_lst)):
+            bkd_lst[i].update(scr)
+            if kkt.rct.colliderect(bkd_lst[i].rct):
+                return
 
         pg.display.update()
-        clock.tick(1000)
-
+        
+        clock.tick(clock_change)
+    
 
 if __name__ == "__main__":
     pg.init()
